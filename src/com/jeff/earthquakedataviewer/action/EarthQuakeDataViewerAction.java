402d4ba29a9cf84eb1c525758f5d01cdd376f7d1
jmathew <jm8thew@gmail.com>
2012-10-12 18:44:38 -0400

    Working JSON data flow

--- src/com/jeff/earthquakedataviewer/action/EarthQuakeDataViewerAction.java
+++ src/com/jeff/earthquakedataviewer/action/EarthQuakeDataViewerAction.java
@@ -13,8 +13,6 @@ import java.util.List;
 
 public class EarthQuakeDataViewerAction extends ActionSupport {
 
-  /* Number used for serialization */
-  private static final long serialVersionUID = 1L; 
   private List earthQuakeList;
 
   /* Entry */
